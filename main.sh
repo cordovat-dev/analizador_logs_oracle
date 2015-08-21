35b055d59ad7564586922f996845a0cdd2d0a5eb
cordovatj <cordovatj@pdvsa.com>
2015-08-21 08:27:05 -0430

    Resumen compacto, ordenación por frecuencia

--- main.sh
+++ main.sh
@@ -13,28 +13,18 @@ function generar_lista_ora {
 	egrep -o "^ORA-[0-9]*" "$1"|sort -u > "$2"
 }
 
-function contar {
+function resumen {
 	for linea in `cat "$1"`; do
 		if [ -z `grep $linea "$LISTAOMITIR"` ]; then
 			CONT=`grep $linea "$2"|wc -l`
-			echo "$linea	$CONT"
+			DESC=`grep $linea "$2"|head -1|cut -d: -f 2|xargs -0`
+			echo "$linea	$CONT	$DESC"
 		fi
 	done
 }
 
-function resumen {
-	for linea in `cat "$1"`; do
-		if [ -z `grep $linea "$LISTAOMITIR"` ]; then
-			DESC=`grep $linea "$2"|head -1`
-			echo "$DESC"
-		fi
-	done	
-}
-
 #generar_lista_omitir
 generar_lista_ora "$LOG" "$LISTAORA"
-#cat "$LISTAORA"
-contar "$LISTAORA" "$LOG"
-resumen "$LISTAORA" "$LOG"
+resumen "$LISTAORA" "$LOG"|sort -k2 -nr
 
 rm "$LISTAORA" "$LISTAOMITIR"
